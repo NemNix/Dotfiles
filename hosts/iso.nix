cf36cee9aa512033c02b52b5ed5f8c0db435febd
NemNix <184756415+NemNix@users.noreply.github.com>
2025-01-08 18:19:29 +0100

    Comment iso config

--- hosts/iso.nix
+++ hosts/iso.nix
@@ -1,22 +1,31 @@
 { pkgs, modulesPath, ... }:
-{
-  imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
 
+{
+  # Base ISO configuration
+  imports = [
+    (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
+  ];
+
+  # Network configuration
   boot.initrd.systemd.network.wait-online.enable = false;
   systemd.services.NetworkManager-wait-online.enable = false;
+
   networking = {
     hostName = "iso";
     firewall.enable = true;
+
     wireless = {
       enable = false;
       iwd.enable = false;
     };
+
     networkmanager = {
       enable = true;
       wifi.backend = "wpa_supplicant";
     };
   };
 
+  # Disable documentation to reduce image size
   documentation = {
     enable = false;
     doc.enable = false;
@@ -26,15 +35,16 @@
     nixos.enable = false;
   };
 
+  # Essential packages
   environment.systemPackages = with pkgs; [
-    yazi
-    helix
+    yazi # Terminal file manager
+    helix # Modern text editor
   ];
 
+  # ISO image configuration
   isoImage = {
     compressImage = true;
     squashfsCompression = null;
     includeSystemBuildDependencies = false;
   };
 }
-
