cdebb88f72a37c9455dc0818aed99e46ee325309
NemNix <184756415+NemNix@users.noreply.github.com>
2025-01-08 16:58:45 +0100

    More Iso config

--- hosts/iso.nix
+++ hosts/iso.nix
@@ -2,13 +2,37 @@
 {
   imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
 
+  boot.initrd.systemd.network.wait-online.enable = false;
+  systemd.services.NetworkManager-wait-online.enable = false;
+  networking = {
+    hostName = "iso";
+    firewall.enable = true;
+    wireless = {
+      enable = false;
+      iwd.enable = false;
+    };
+    networkmanager = {
+      enable = true;
+      wifi.backend = "wpa_supplicant";
+    };
+  };
+
+  documentation = {
+    enable = false;
+    doc.enable = false;
+    man.enable = false;
+    dev.enable = false;
+    info.enable = false;
+    nixos.enable = false;
+  };
+
   environment.systemPackages = with pkgs; [
     yazi
     helix
   ];
 
   isoImage = {
-    compressImage = false;
+    compressImage = true;
     squashfsCompression = null;
     includeSystemBuildDependencies = false;
   };
